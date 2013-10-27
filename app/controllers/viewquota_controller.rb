a1f6bd3db56a596aeb399ddd2a65757ba14c417b
saasbook <saasbook@saasbook.(none)>
2013-10-27 15:10:15 -0700

    updated cucumber stuff

--- app/controllers/viewquota_controller.rb
+++ app/controllers/viewquota_controller.rb
@@ -3,3 +3,4 @@ class ViewQuotaController < ApplicationController
   def index
     @storage_data = {"Bob" => {"proj1" => 0.97, "proj2" => 0.45, "proj3" => 0.28}}
   end
+end
--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,14 +1,14 @@
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following directories exist/ do |directory_table|
-  directory_table.hashes.each do |directory|
-      Directory.create! directory
-  end
+  #directory_table.hashes.each do |directory|
+   #   Directory.create! directory
+  #end
 end
 
-Given /And I am on the dashboard page/ do |page_name|
-  visit path_to(page_name)
-end
+#Given /And I am on the dashboard page/ do |page_name|
+ # visit path_to(page_name)
+#end
 
 
 When /^(?:|I )follow '(.*)'$/ do |link|
@@ -19,6 +19,3 @@ end
 
 
 
-Then /I should see all percentages_of_usage/ do
-
-end
