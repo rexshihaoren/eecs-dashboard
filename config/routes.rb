b66d522b70bf72e9788dd65f5f2810d45787755a
saasbook <saasbook@saasbook.(none)>
2013-10-27 23:01:11 -0700

    Little bit of cleaning, fixed rottenpotatoes => dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rottenpotatoes::Application.routes.draw do
-  resources :movies
+  resources :dashboard
   resources :viewquota
 
   get '/viewquotas/', :controller => 'viewquotas', :action => 'index'
-  #redirect(get '/dashboard/', :controller => 'dashboards', :action => 'index')
   root :to => redirect('/dashboard')
-  #root :to => get '/viewquotas'
 end
