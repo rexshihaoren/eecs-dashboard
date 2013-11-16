1fc12862562e8c13385e6a309c1e39f9579f2880
rexshihaoren <renshihao@gmail.com>
2013-11-15 20:32:48 -0800

    cucumber done

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -6,3 +6,11 @@ Factory.define :directory do |d|
   d.quota 10
 end
 
+Factory.define :usage do |u|
+	u.user = 'fox'
+	u.directory = 'proj1'
+	u.date = '11/11/13'
+	usage = 100
+	rate = 10
+
+
--- spec/models/usage_spec.rb
+++ spec/models/usage_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Usage do
-  pending "add some examples to (or delete) #{__FILE__}"
+  	before do 
+		usage=Factory(:usage)
+	end
+
+	it 'should include directory, usage, rate, and date' do
+		usage.user ='fox'
+		usage.usage == 100
+		usage.rate == 10
+		usage.date == '11/11/13'
+	end
 end
