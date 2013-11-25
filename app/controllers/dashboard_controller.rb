ed36a3ab739cd1a2e8517bda180d5b6f403c9e9a
Liz <liz@berkeley.edu>
2013-11-24 22:01:07 -0800

    Little beautification

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
   def index 
 	@isDependant = params[:dependant] != NIL
+	@user_name = "aculich";  #hardcoded for now, later we need to do a modularized structure
 	if(@isDependant)
 	        session[:dependant] = params[:dependant]
 		@myName = session
