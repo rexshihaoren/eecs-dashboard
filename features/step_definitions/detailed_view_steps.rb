c11536b47eb73fdefab4ead0a7b554114aeda0c2
rexshihaoren <renshihao@gmail.com>
2013-12-01 19:24:57 -0800

    added new feature cucumber and new rspec for viewbilingovertime

--- features/step_definitions/detailed_view_steps.rb
+++
@@ -1,3 +0,0 @@
-Then /I should see usage history for "(.*)"$/ do |directory|
-  pending # express the regexp above with the code you wish you had
-end
--- features/step_definitions/directories_steps.rb
+++
@@ -1,5 +0,0 @@
-
-
-Given /the following directories exist:$/ do |directories|
-	Directory.create!(directories.hashes)
-end
